28fc698883672f805b303f3b350ddfee355da10f
Justin Jansen <jjansen@databay.de>
2023-09-21 14:24:13 +0200

    added image cropper to boost perfomanc

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import {Game} from "../hooks/useGames";
 import {Card, CardBody, Heading, Image, CardFooter, HStack} from "@chakra-ui/react";
 import {PlatformIconList} from "./PlatformIconList";
 import {CriticScore} from "./CriticScore";
+import getCroppedImageUrl from '../services/ImageCropper';
 
 interface Props {
     game: Game
@@ -10,7 +11,7 @@ interface Props {
 export const GameCard = ({game}: Props) => {
     return (
         <Card borderRadius={10} overflow="hidden">
-            <Image src={game.background_image}/>
+            <Image src={getCroppedImageUrl(game.background_image)}/>
             <CardBody>
                 <Heading fontSize='2xl'>{game.name}</Heading>
             </CardBody>
