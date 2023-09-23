0669165807b3914d297fece97eebee0f29522475
Justin Jansen <jjansen@databay.de>
2023-09-23 14:00:12 +0200

    added handling games without an image

--- src/services/ImageCropper.ts
+++ src/services/ImageCropper.ts
@@ -1,5 +1,9 @@
 
+import noImage from '../assets/no-image-placeholder-6f3882e0.webp';
+
 const getCroppedImageUrl = (url: string) => {
+    if (!url) return noImage;
+
     const target = 'media/';
 
     const index = url.indexOf(target) + target.length;
